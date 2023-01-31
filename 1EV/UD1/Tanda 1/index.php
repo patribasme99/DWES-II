047ba2b01fb2f53531621c8a0f8055b7f07ce999
patribasme99 <61690831+patribasme99@users.noreply.github.com>
2023-01-31 16:30:14 +0100

    Create index.php

---
+++ 1EV/UD1/Tanda 1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <p><a href="01.php">Ejercicio 1</a></p>
+    <p><a href="02.php">Ejercicio 2</a></p>
+    <p><a href="03.php">Ejercicio 3</a></p>
+    <p><a href="04.php">Ejercicio 4</a></p>
+    <p><a href="05.php">Ejercicio 5</a></p>
+    <p><a href="06.php">Ejercicio 6</a></p>
+    <p><a href="07.php">Ejercicio 7</a></p>
+</body>
+</html>
